5e9678ecadb910dfe5205ab0666f57d2f74affef
Torangto Situngkir <torangsitungkir93@gmail.com>
2023-02-18 18:39:27 +0700

    mvc

--- 1.Instalasi/routes/web.php
+++ 1.Instalasi/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -30,50 +33,14 @@ Route::get('/about', function () {
 
 
 Route::get('/blog', function () {
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Torangto Situngkir",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure recusandae hic fugit a corrupti eius magnam dicta, soluta dolorum, mollitia nobis. Molestias maxime earum distinctio qui repellendus fugit porro deleniti excepturi impedit ea dolorem, fuga eligendi deserunt temporibus. Tempora deserunt quasi inventore quas officiis rem? Saepe voluptatibus modi recusandae quo temporibus! Ab, sunt. Harum dignissimos recusandae, dolorem modi ex sed magni eum, quasi unde commodi, obcaecati minus voluptatum vero fugiat esse excepturi minima doloremque quos nostrum? Ut vero repellat architecto."
-        ],[
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Erik Cantona",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure recusandae hic fugit a corrupti eius magnam dicta, soluta dolorum, mollitia nobis. Molestias maxime earum distinctio qui repellendus fugit porro deleniti excepturi impedit ea dolorem, fuga eligendi deserunt temporibus. Tempora deserunt quasi inventore quas officiis rem? Saepe voluptatibus modi recusandae quo temporibus! Ab, sunt. Harum dignissimos recusandae, dolorem modi ex sed magni eum, quasi unde commodi, obcaecati minus voluptatum vero fugiat esse excepturi minima doloremque quos nostrum? Ut vero repellat architecto."
-        ],
-    ];
+
     return view('posts',[
         "title" => "Posts",
-        "posts" => $blog_posts,
+        "posts" => Post::all(),
     ]);
 });
+
+Route::get('/posts',[PostController::class,'index']);
 
 // Halaman single post
-Route::get('posts/{slug}',function($slug){
-
-    $blog_posts = [
-        [
-            "title" => "Judul Post Pertama",
-            "slug" => "judul-post-pertama",
-            "author" => "Torangto Situngkir",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure recusandae hic fugit a corrupti eius magnam dicta, soluta dolorum, mollitia nobis. Molestias maxime earum distinctio qui repellendus fugit porro deleniti excepturi impedit ea dolorem, fuga eligendi deserunt temporibus. Tempora deserunt quasi inventore quas officiis rem? Saepe voluptatibus modi recusandae quo temporibus! Ab, sunt. Harum dignissimos recusandae, dolorem modi ex sed magni eum, quasi unde commodi, obcaecati minus voluptatum vero fugiat esse excepturi minima doloremque quos nostrum? Ut vero repellat architecto."
-        ],[
-            "title" => "Judul Post Kedua",
-            "slug" => "judul-post-kedua",
-            "author" => "Erik Cantona",
-            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure recusandae hic fugit a corrupti eius magnam dicta, soluta dolorum, mollitia nobis. Molestias maxime earum distinctio qui repellendus fugit porro deleniti excepturi impedit ea dolorem, fuga eligendi deserunt temporibus. Tempora deserunt quasi inventore quas officiis rem? Saepe voluptatibus modi recusandae quo temporibus! Ab, sunt. Harum dignissimos recusandae, dolorem modi ex sed magni eum, quasi unde commodi, obcaecati minus voluptatum vero fugiat esse excepturi minima doloremque quos nostrum? Ut vero repellat architecto."
-        ],
-    ];
-
-    $new_post = [];
-    foreach ($blog_posts as $post) {
-        if($post['slug'] === $slug){
-            $new_post = $post;
-        }
-    }
-    return view('post',[
-        "title" => "Single Post",
-        "post" => $new_post,
-    ]);
-});
+Route::get('posts/{slug}',[PostController::class,'show']);
