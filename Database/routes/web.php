4a90f03f4842c3170e0fdd126b861eead84b530b
Torangto Situngkir <torangsitungkir93@gmail.com>
2023-03-05 08:00:16 +0700

    n+1 problem

--- Database/routes/web.php
+++ Database/routes/web.php
@@ -60,13 +60,13 @@ Route::get('categories',function(){
 Route::get('categories/{category:slug}',function(Category $category){
     return view('posts',[
         'title' => "Post by Category : $category->name",
-        'posts' => $category->posts,
+        'posts' => $category->posts->load('category','author'),
     ]);
 });
 
 Route::get('/authors/{author:username}',function(User $author){
     return view('posts',[
         'title' => "Post by Author : $author->name",
-        'posts' => $author->posts,
+        'posts' => $author->posts->load('category','author'),
     ]);
 });
