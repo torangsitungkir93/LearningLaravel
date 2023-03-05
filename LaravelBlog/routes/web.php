f00c27c3aca23b578103fcd77ad389285481882c
Torangto Situngkir <torangsitungkir93@gmail.com>
2023-03-05 21:52:15 +0700

    login

--- LaravelBlog/routes/web.php
+++ LaravelBlog/routes/web.php
@@ -1,11 +1,12 @@
 <?php
 
-use App\Http\Controllers\PostController;
 use App\Models\Post;
-use Illuminate\Support\Facades\Route;
 
 use App\Models\Category;
-use App\Models\User;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 
 /*
@@ -68,3 +69,7 @@ Route::get('categories/{category:slug}',function(Category $category){
     ]);
 });
 
+
+Route::get('/login',[LoginController::class,'index']);
+
+Route::get('/register',[RegisterController::class,'index']);
